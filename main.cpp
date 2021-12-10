c1a1ba028073c46d84d64abb8f6400a210a87798
IvanGrishyn <IvanGrishyn@gmail.com>
2021-12-10 23:26:00 +0200

    изменено:      main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
+#include <stdio.h>
+
+class vec {
+public:	float x;
+	float y;
+	float z;
+	vec();
+	vec add(vec a,vec b );
+	void print(){printf("x=%3.3f y=%3.3f z=%3.3f\n",this->x,this->y,this->z);}
+	  
+	  friend vec operator+( vec a, vec b);
+	  
+	  };
 
 
+vec::vec(){this->x=1;this->y=2;this->z=3;}
+
+vec vec::add(vec a, vec b){
+this->x+=a.x;
+this->y+=a.y;
+this->z+=a.z;
+return *this;
+}
+
+
+vec operator+(vec a, vec b)
+{a.x+=b.x;
+a.y+=b.y;
+a.z+=b.z;
+return a;}
 
 int main (){
-
+vec a,b,c;
+a=b+c+a;
+c.print();
+b.print();
+a.print();
 return 0;
 }
